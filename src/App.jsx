3526de2ed7a088cfd5292af294ddfa9398e76da5
scottwillow84 <scott.willow84@gmail.com>
2026-08-13 17:31:46 +1000

    Simplify site to single family trip planner

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Itinerary from "./pages/Itinerary";
-import Crew from "./pages/Crew";
-import Vehicle from "./pages/Vehicle";
-import Places from "./pages/Places";
-import Accommodation from "./pages/Accommodation";
-import Feedback from "./pages/Feedback";
 
 export default function App() {
   return (
     <BrowserRouter basename="/willow-expeditions-v2">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/timeline" element={<Itinerary />} />
-        <Route path="/crew" element={<Crew />} />
-        <Route path="/places" element={<Places />} />
-        <Route path="/accommodation" element={<Accommodation />} />
-        <Route path="/vehicle" element={<Vehicle />} />
-        <Route path="/feedback" element={<Feedback />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
