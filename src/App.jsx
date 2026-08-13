ca98418dc3f6e054674d3629025aca2ecd3a1e9e
scottwillow84 <scott.willow84@gmail.com>
2026-08-13 18:00:23 +1000

    Add route comparison page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
+import RouteDetail from "./pages/RouteDetail";
 
 export default function App() {
   return (
     <BrowserRouter basename="/willow-expeditions-v2">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/route/hadspen-lulworth" element={<RouteDetail />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
