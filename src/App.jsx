ddbb99a7b1c7168b6f466130cd679064d10cdd85
scottwillow84 <scott.willow84@gmail.com>
2026-08-13 19:17:11 +1000

    Fix GitHub Pages route navigation with hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import RouteDetail from "./pages/RouteDetail";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/willow-expeditions-v2">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/route/hadspen-lulworth" element={<RouteDetail />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
