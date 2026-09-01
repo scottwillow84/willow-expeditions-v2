c1b41d6cb4e4e952a590a510411f793ec7db2d99
scottwillow84 <scott.willow84@gmail.com>
2026-09-01 21:00:11 +1000

    Add stay detail routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import RouteDetail from "./pages/RouteDetail";
+import StayDetail from "./pages/StayDetail";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/route/:routeId" element={<RouteDetail />} />
+        <Route path="/stay/:stayId" element={<StayDetail />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
